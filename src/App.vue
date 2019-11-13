dbe4d5528137d298646a0236c7060a4cd85b82d0
Uma Maheswari Chandrabose <39375672+UmaMaheswariChandrabose@users.noreply.github.com>
2019-11-13 19:14:16 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <div style="margin:10% 25%;">
+        <br>
+        <ejs-multiselect :dataSource='remoteData' 
+        :fields='remoteFields' placeholder='Select a name' 
+        sortOrder='Descending'>
+        </ejs-multiselect>
+      </div>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import{ MultiSelectPlugin } from '@syncfusion/ej2-vue-dropdowns';
+Vue.use(MultiSelectPlugin);
+import{ DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
+var remoteDataSource = new DataManager({
+   url: 'https://ej2services.syncfusion.com/production/web-services/api/Employees',
+   adaptor: new WebApiAdaptor,
+   crossDomain: true
+});
+export default Vue.extend({
+  data: function() {
+    return {
+      remoteData: remoteDataSource,
+      remoteFields: {text: 'FirstName',  value: 'EmployeeID'},
+      localData:[
+        { Id: 'game1', Game: 'Football' },
+        { Id: 'game2', Game: 'Basketball' },
+        { Id: 'game3', Game: 'Volleyball' }
+      ],
+      localFields:{value: 'Id', text: 'Game' }
+    };
+  }
+});
+
+</script>
+<style>
+@import url(https://cdn.syncfusion.com/ej2/material.css);
+</style>
